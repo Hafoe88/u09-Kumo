2d8c294fe6df60064338889803a92cae2fa60d04
Hafiz Samaae <hafez@hotmail.com>
2023-11-15 20:20:40 +0100

    fix

--- Kumo-be/index.js
+++ Kumo-be/index.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 
 app.use(cors({
   credentials: true,
-  origin: 'https://u09-kumo-be.vercel.app',
+  origin: 'https://u09-kumo-fe.vercel.app/',
   exposedHeaders: ['Access-Control-Allow-Origin']
 }));
 
