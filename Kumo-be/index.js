c2ca0f3a3c5cfd899268d0274a6877b609067cb6
Hafiz Samaae <hafez@hotmail.com>
2023-11-16 15:22:16 +0100

    update

--- Kumo-be/index.js
+++ Kumo-be/index.js
@@ -45,7 +45,7 @@ async function getUserDataFromRequest(req) {
 
 // test
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://u09-kumo-fe.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', true);
