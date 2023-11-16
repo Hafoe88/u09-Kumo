0aa361905c355144b3f54d7a5dbe498afe38dcac
Hafiz Samaae <hafez@hotmail.com>
2023-11-16 10:58:02 +0100

    fix

--- Kumo-fe/src/Chat.jsx
+++ Kumo-fe/src/Chat.jsx
@@ -19,7 +19,7 @@ export default function Chat() {
     connectToWs();
   }, [selectedUserId]);
   function connectToWs() {
-    const ws = new WebSocket("ws://https://u09-kumo-be.vercel.app/profile");
+    const ws = new WebSocket("ws://https://u09-kumo-be.vercel.app/");
     setWs(ws);
     ws.addEventListener("message", handleMessage);
     ws.addEventListener("close", () => {
